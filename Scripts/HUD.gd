9a1cd30ea6df803fd41eb9bbe96deff0f71e3bdf
Artur023 <artur.023@mail.ru>
2023-08-02 09:47:17 +0400

    feat: актуальная версия, которая у меня

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -10,7 +10,6 @@ func show_message(text):
 func show_game_over():
 	show_message('Game Over')
 	yield($MessageTimer, "timeout")
-	
 	$Message.text = "Ещё \n разок?"
 	$Message.show()
 	yield(get_tree().create_timer(1), "timeout")
